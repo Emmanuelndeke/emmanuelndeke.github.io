92dbe101d2c95384aa23bc7ce547463b097f3905
Emmanuelndeke <emmanuelndeke2024@gmail.com>
2026-04-13 17:09:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ lightboxImg.src = map.src;
 lightbox.addEventListener('click',()=>{
 lightbox.style.display="none";
 });
+const toggle = document.getElementById("menuToggle");
+const nav = document.getElementById("navLinks");
+
+toggle.addEventListener("click",()=>{
+nav.classList.toggle("show");
+});
