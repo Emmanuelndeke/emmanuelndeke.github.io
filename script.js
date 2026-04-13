dd9db7d4756a32a38ecbd0026027d29ecd0a56fc
Emmanuelndeke <emmanuelndeke2024@gmail.com>
2026-04-13 15:40:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ entry.target.classList.add('visible');
 sections.forEach(section => {
 observer.observe(section);
 });
+const maps = document.querySelectorAll('.map-image');
+const lightbox = document.getElementById('mapLightbox');
+const lightboxImg = document.getElementById('lightboxImage');
+
+maps.forEach(map=>{
+map.addEventListener('click',()=>{
+lightbox.style.display="flex";
+lightboxImg.src = map.src;
+});
+});
+
+lightbox.addEventListener('click',()=>{
+lightbox.style.display="none";
+});
